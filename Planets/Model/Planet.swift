23da79ba82e374b3ae5814a7bc7b5dc63b073ed1
Ben Gohlke <jcgohlke@me.com>
2019-06-13 14:16:13 -0400

    Moved properties.

--- Planets/Model/Planet.swift
+++ Planets/Model/Planet.swift
@@ -10,11 +10,11 @@ import UIKit
 
 struct Planet {
     
+    var name: String
+    var image: UIImage
+    
     init(name: String, imageName: String) {
         self.name = name
         self.image = UIImage(named: imageName)!
     }
-    
-    var name: String
-    var image: UIImage
 }
