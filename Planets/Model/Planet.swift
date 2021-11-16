71eea7bd10477e3934b43565a3ffb7afc42a1f76
Bob Lauer <bob@lambdaschool.com>
2021-11-16 16:14:32 -0600

    BloomTech

--- Planets/Model/Planet.swift
+++ Planets/Model/Planet.swift
@@ -3,7 +3,7 @@
 //  Planets
 //
 //  Created by Andrew R Madsen on 8/2/18.
-//  Copyright © 2018 Lambda Inc. All rights reserved.
+//  Copyright © 2021 Bloom Institute of Technology Inc. All rights reserved.
 //
 
 import UIKit
